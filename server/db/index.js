1de8dcffe60bda63daa1c5571db0e646407d78af
leokyeung <leoyeung99@gmail.com>
2019-11-13 20:40:14 -0800

    Updated

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,14 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'plantlife'
+});
 
+con.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected!');
+});
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,18 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      db.connect();
+      var queryString = 'select * from '
+
+    }, // a function which produces all the messages
+    post: function () { } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () { },
+    post: function () { }
   }
 };
 
